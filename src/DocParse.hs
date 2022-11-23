0e324c6d628a188be0a6d8c9dc8cb59a17c8030a
Joy Liu <joyliu.q@gmail.com>
2022-11-22 21:11:45 -0500

    :white_check_mark: Add DocParse tests

--- src/DocParse.hs
+++ src/DocParse.hs
@@ -89,6 +89,19 @@ test_tagP =
 classP :: Parse JavaDocComment
 classP = undefined
 
+-- TODO: Public vs private classes?
+test_classP :: Test
+test_classP = TestList [
+    P.parse classP "class Foo {}" ~?= Right (Class (Name "Foo") (Description "") []),
+    P.parse classP "public class Foo {}" ~?= Right (Class (Name "Foo") (Description "") []),
+    P.parse classP "private class Foo { }" ~?= Right (Class (Name "Foo") (Description "") []),
+    P.parse classP "class Foo { /* blah */ }" ~?= Right (Class (Name "Foo") (Description "") []),
+    P.parse classP "class Foo { \n // blah \n }" ~?= Right (Class (Name "Foo") (Description "") []),
+    P.parse classP "class Foo { \n // blah \n } \n" ~?= Right (Class (Name "Foo") (Description "") []),
+    P.parse classP "/** The Foo class \n * @version 1.0 \n */ \n class Foo { \n // blah \n } \n" ~?= Right (Class (Name "Foo") (Description "The Foo class") [Version (Description "1.0")]),
+    P.parse classP "/** The Foo class \n * @version 1.0 \n * @param x the x value \n */ \n class Foo { \n // blah \n } \n" ~?= Right (Class (Name "Foo") (Description "The Foo class") [Version (Description "1.0"), Param (Name "x") (Description "the x value")])
+  ]
+
 interfaceMethodP :: Parse JavaDocComment
 interfaceMethodP = undefined
 
@@ -98,11 +111,45 @@ classMethodP = undefined
 methodP :: Parse JavaDocComment
 methodP = wsP (interfaceMethodP <|> classMethodP)
 
+test_methodP :: Test
+test_methodP = TestList [
+    P.parse interfaceMethodP "interface Foo { void bar(); }" ~?= Right (Method (Name "bar") (Description "") []),
+    P.parse interfaceMethodP "class Foo { public void bar(); }" ~?= Right (Method (Name "bar") (Description "") []),
+    P.parse interfaceMethodP "interface Foo { private void bar(); }" ~?= Right (Method (Name "bar") (Description "") []),
+    P.parse interfaceMethodP "interface Foo { void bar(); /* blah */ }" ~?= Right (Method (Name "bar") (Description "") []),
+    P.parse interfaceMethodP "class Foo { void bar(); \n // blah \n }" ~?= Right (Method (Name "bar") (Description "") []),
+    P.parse interfaceMethodP "interface Foo { void bar(); \n // blah \n } \n" ~?= Right (Method (Name "bar") (Description "") []),
+    P.parse interfaceMethodP "class Foo { /** The bar method \n * @version 1.0 \n */ \n void bar(); \n // blah \n } \n" ~?= Right (Method (Name "bar") (Description "The bar method") [Version (Description "1.0")]),
+    P.parse interfaceMethodP "interface Foo { /** The bar method \n * @version 1.0 \n * @param x the x value \n */ \n void bar(); \n // blah \n } \n" ~?= Right (Method (Name "bar") (Description "The bar method") [Version (Description "1.0"), Param (Name "x") (Description "the x value")])
+  ]
+
 interfaceP :: Parse JavaDocComment
 interfaceP = undefined
 
+test_interfaceP :: Test
+test_interfaceP = TestList [
+    P.parse interfaceP "interface Foo {}" ~?= Right (Interface (Name "Foo") (Description "") []),
+    P.parse interfaceP "public interface Foo {}" ~?= Right (Interface (Name "Foo") (Description "") []),
+    P.parse interfaceP "private interface Foo { }" ~?= Right (Interface (Name "Foo") (Description "") []),
+    P.parse interfaceP "interface Foo { /* blah */ }" ~?= Right (Interface (Name "Foo") (Description "") []),
+    P.parse interfaceP "interface Foo { \n // blah \n }" ~?= Right (Interface (Name "Foo") (Description "") []),
+    P.parse interfaceP "interface Foo { \n // blah \n } \n" ~?= Right (Interface (Name "Foo") (Description "") []),
+    P.parse interfaceP "/** The Foo interface \n * @version 1.0 \n */ \n interface Foo { \n // blah \n } \n" ~?= Right (Interface (Name "Foo") (Description "The Foo interface") [Version (Description "1.0")]),
+    P.parse interfaceP "/** The Foo interface \n * @version 1.0 \n * @param x the x value \n */ \n interface Foo { \n // blah \n } \n" ~?= Right (Interface (Name "Foo") (Description "The Foo interface") [Version (Description "1.0"), Param (Name "x") (Description "the x value")])
+  ]
+
 enumP :: Parse JavaDocComment
 enumP = undefined
+
+test_enumP :: Test
+test_enumP = TestList [
+    P.parse enumP "enum Foo {}" ~?= Right (Enum (Name "Foo") (Description "")),
+    P.parse enumP "public enum Foo {}" ~?= Right (Enum (Name "Foo") (Description "")),
+    P.parse enumP "private enum Foo { }" ~?= Right (Enum (Name "Foo") (Description "")),
+    P.parse enumP "enum Foo { /* blah */ }" ~?= Right (Enum (Name "Foo") (Description "")),
+    P.parse enumP "enum Foo { \n // blah \n }" ~?= Right (Enum (Name "Foo") (Description "")),
+    P.parse enumP "enum Foo { \n // blah \n } \n" ~?= Right (Enum (Name "Foo") (Description ""))
+  ]
 
 javaDocCommentP :: Parse JavaDocComment
 javaDocCommentP = wsP (classP <|> interfaceP <|> enumP <|> methodP)
